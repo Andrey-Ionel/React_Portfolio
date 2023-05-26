27b1e6975f45a8f5483b613ba298692b3caa877a
Andrey-Ionel <a-ionel@ukr.net>
2023-05-26 12:43:00 +0300

    deploy

--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -9,7 +9,7 @@ const config: Configuration = {
   mode: 'production',
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'docs'),
     filename: '[name].[contenthash].js',
     publicPath: '',
   },
